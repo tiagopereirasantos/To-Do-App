63667125b5d83a3b6d57d93e7b4705987daa2042
Tiago Pereira Santos <tiago03081997@gmail.com>
2022-11-21 23:12:40 -0300

    Iniciando a conexao com o banco de dados

---
+++ TodoApp/app/src/main/java/util/ConnectionFactory.java
@@ -0,0 +1,11 @@
+
+package util;
+
+
+public class ConnectionFactory {
+ 
+    public static final String DRIVER = "com.mysql.fdbc.Driver";
+    public static final String URL = "jdbc:mysql://localhost:3306/todoApp";
+    public static final String USER = "root";
+    public static final String PASS = "" ;
+}
