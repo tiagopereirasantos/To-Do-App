8be773e8b194966a878ca9982ac38c703b866484
Tiago Pereira Santos <tiago03081997@gmail.com>
2022-11-27 23:31:18 -0300

    Método update da classe TaskController finalizada

--- TodoApp/app/src/main/java/controler/TaskController.java
+++ TodoApp/app/src/main/java/controler/TaskController.java
@@ -48,6 +48,38 @@ public class TaskController  {
     
     public void update(Task task){
         
+        String sql = "UPDATE tasks SET "
+                + "idProject = ?,"
+                + "name = ?,"
+                + "description = ?,"
+                + "notes = ?,"
+                + "completed = ?,"
+                + "deadline = ?,"
+                + "createdAt = ?,"
+                + "updatedAt = ?) "
+                + "WHERE id = ?";
+        
+        Connection connection = null;
+        PreparedStatement statement = null;
+        
+        try{
+            connection = ConnectionFactory.getConnection();
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1, task.getIdProject());
+            statement.setString(2, task.getName());
+            statement.setString(3, task.getDescription());
+            statement.setString(4, task.getNotes());
+            statement.setBoolean(5, task.isIsCompleted());
+            statement.setDate(6, new Date(task.getDeadline().getTime()));
+            statement.setDate(7, new Date(task.getCreatedAt().getTime()));
+            statement.setDate(8 , new Date(task.getUpdatedAt().getTime()));
+            statement.execute();
+        } catch (Exception ex){
+            throw new RuntimeException("Erro ao atualizar tarefa" + ex.getMessage(), ex);
+        } finally{
+            ConnectionFactory.closeConnection(connection, statement);
+        }
+        
     }
     
     public void removeById(int taskId){
@@ -65,9 +97,7 @@ public class TaskController  {
         } catch(Exception ex){
             throw new RuntimeException("Erro ao deletar a tarefa" + ex.getMessage(),ex);
         } finally{
-            ConnectionFactory.closeConnection(connection, statement);
-            
-           
+            ConnectionFactory.closeConnection(connection, statement); 
         }
         
     }
